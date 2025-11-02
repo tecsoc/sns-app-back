365e2f619129ca8b29bc0c506a911743eef0e7d6
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2025-11-02 18:15:06 +0900

    chore: generate User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :posts, format: 'json'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+    screen_name { "MyString" }
+    password { "MyString" }
+  end
+end
