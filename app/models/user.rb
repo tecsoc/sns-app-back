b7d961a23584837edd723ded8e549d14bfb22b79
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2025-11-08 20:00:55 +0900

    feat: パスワードのバリデーションと暗号化を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :password,
+    format: {
+      with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d\W]{8,}\z/,
+      message: 'は8文字以上で、大文字・小文字・数字をそれぞれ1文字以上含めてください'
+    },
+    if: -> { password.present? }  # 編集時に空で通すため
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_03_015719) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_08_105723) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
@@ -23,5 +23,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_03_015719) do
     t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 end
